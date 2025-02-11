726d90696dfd34245daeae3e9bea96b97a10954f
Iván Pérez <ivan.perez@cancunit.com>
2025-02-10 22:59:35 -0500

    Iniciando Automatización en Playwright

---
+++ tests/loginTests.spec.ts
@@ -0,0 +1,17 @@
+import { test } from '@playwright/test';
+import { LoginPage } from './pageobjects/loginPage';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://mariturwebapp.azurewebsites.net');
+});
+
+test('loginTest', async ({ page }) => {
+ 
+  const loginPage = new LoginPage(page)
+  
+  loginPage.loginEmailAndPassword('ivan.perez@cancunit.com','Dotnet_1')
+  await page.waitForTimeout(6000);
+  loginPage.checkSuccessfulLogin();
+
+});
+
