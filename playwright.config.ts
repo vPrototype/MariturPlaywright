7bc265519453a939edd30f5a0f3bfe7973de0a97
Iván Pérez <ivan.perez@cancunit.com>
2025-03-12 11:45:56 -0500

    Optimización de config y yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,66 +13,53 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   globalSetup: require.resolve('./config/globalSetup.ts'),
+
+  // Configuración del reporter
   reporter: [
     ['list'],
-    ['allure-playwright']
-  ],  //'html', <-- antigua configuración antes de instalar allure reports
+    ['allure-playwright', {
+      outputFolder: 'allure-results', // Definir el folder de resultados
+      suiteTitle: true, // Mostrar el título de la suite en el reporte
+      details: true // Mostrar detalles de la prueba en el reporte
+    }]
+  ],
+
+  // Configuración del entorno de pruebas
   use: {
-    headless: process.env.CI ? true : false,
-    //headless: false, // Desactiva el modo headless para todos los proyectos
-    baseURL: process.env.BASE_URL,
-    //trace: 'on-first-retry', <-- antigua configuración antes de instalar allure
-    screenshot: 'on',
-    video: 'retain-on-failure',
-    trace: 'retain-on-failure',
+    headless: process.env.CI ? true : false,  // Dependiendo del entorno
+    baseURL: process.env.BASE_URL,  // Base URL desde las variables de entorno
+    screenshot: 'on',  // Tomar capturas en cada paso
+    video: 'retain-on-failure',  // Guardar video solo si falla la prueba
+    trace: 'retain-on-failure',  // Guardar el trace solo si falla
   },
-  /* Configure projects for major browsers */
+
+  // Configuración de proyectos (usualmente para múltiples navegadores)
   projects: [
     {
       name: 'setup',
-      testMatch: /global\.setup\.ts/,
+      testMatch: /global\.setup\.ts/,  // Configuración global
     },
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-      dependencies: ['setup'],
+      dependencies: ['setup'],  // Asegura que la configuración global se ejecute antes
     },
-
+    // Habilitar otros navegadores si es necesario
     /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    },*/
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  /* Configuración de servidor local si es necesario
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+  */
 });
